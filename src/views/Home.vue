37ac3ca8fc97a9d7136d8cd93ab86146b60d0b88
yahu39 <yahu39pc@gmail.com>
2021-02-24 08:17:57 -0500

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <px-assets-table :assets="assets" />
+  </div>
+</template>
+
+<script>
+import api from '@/api'
+import PxAssetsTable from '@/components/PxAssetsTable'
+
+export default {
+  name: 'Home',
+  components: { PxAssetsTable },
+  data() {
+    return {
+      assets: []
+    }
+  },
+  created() {
+    api.getAssets().then(assets => (this.assets = assets))
+  }
+}
+</script>
