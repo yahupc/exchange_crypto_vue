6285cc37eb31d476cbd8a3431b2628bbf6e5ce72
yahu39 <yahu39pc@gmail.com>
2021-02-27 02:54:58 -0500

    Filtro Buscar

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -10,12 +10,20 @@
         <th>Precio</th>
         <th>Cap. de Mercado</th>
         <th>Variación 24hs</th>
-        <td class="hidden sm:block"></td>
+        <td class="hidden sm:block">
+          <input
+            class="bg-gray focus:outline-noe order-b border-gray-400 py2 px4 block w-full appearance-none leading-normal"
+            id="filter"
+            placeholder="Buscar..."
+            type="text"
+            v-model="filter"
+          />
+        </td>
       </tr>
     </thead>
     <tbody>
       <tr
-        v-for="a in assets"
+        v-for="a in filteredAssets"
         :key="a.id"
         class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
       >
@@ -70,10 +78,28 @@ export default {
   name: 'PxAssetsTable',
   components: { PxButton },
 
+  data() {
+    return {
+      filter: ''
+    }
+  },
+
   props: {
     assets: {
       type: Array,
       default: () => []
+    }
+  },
+  computed: {
+    filteredAssets() {
+      if (!this.filter) {
+        return this.assets
+      }
+      return this.assets.filter(
+        a =>
+          a.symbol.toLowerCase().includes(this.filter.toLowerCase()) ||
+          a.name.toLowerCase().includes(this.filter.toLowerCase())
+      )
     }
   },
   methods: {
